d2c94b8e6496649d44d3f985e2584122eeb07399
Jorge Daniel Sequeira Matias <jdsm@tecnico.ulisboa.pt>
2019-08-20 08:53:19 +0100

    Added Debian vs Raspbian detection to set correct lib dirs.

--- configure_debian.sh
+++ configure_debian.sh
@@ -13,4 +13,26 @@ then
 	JAVAINCPATH=${JAVACPATH%/*}
 	JAVAINCPATH=$JAVAINCPATH/../include
 fi
-./configure --lib+=-L${QT4PATH}/lib/ --lib+=-L/usr/lib/x86_64-linux-gnu/ --include+=${QT4PATH}/include/Qt --include+=${JAVAINCPATH} --include+=${JAVAINCPATH}/linux  BUILD_SDK=1
+
+[ -f /etc/os-release ] && . /etc/os-release
+
+if [[ "x${ID}" == "x" ]]; then
+	echo "Couldn't find the Debian distribution flavor" >&2
+	exit 1
+fi
+
+case $ID in
+	"debian")
+		LIBDIR="/usr/lib/x86_64-linux-gnu"	# Assuming x86 64bit only
+		;;
+	"raspbian")
+		LIBDIR="/usr/lib/arm-linux-gnueabihf/"  # Assuming just armhf (32bit) for now
+		;;
+	*)
+		LIBDIR="/usr/lib"
+		;;
+esac
+	
+	
+
+./configure --lib+=-L${QT4PATH}/lib/ --lib+=-L/usr/lib/arm-linux-gnueabihf/ --include+=${QT4PATH}/include/Qt --include+=${JAVAINCPATH} --include+=${JAVAINCPATH}/linux  BUILD_SDK=1
